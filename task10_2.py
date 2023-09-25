f243c02929606f00fb8ce0470814854a5262be67
MeruyertA <145113292+MeruyertA@users.noreply.github.com>
2023-09-25 23:44:31 +0600

    Delete task10_2.py

--- task10_2.py
+++
@@ -1,9 +0,0 @@
-a = int(input())
-b = int(input())
-c = int(input())
-d = int(input())
-
-if (a == c) or (b == d) or (a - c == b - d) or (a - c == -(b - d)):
-    print("YES")
-else:
-    print("NO")
