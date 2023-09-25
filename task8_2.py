daab3d5673ab574544d01c96d898dec541e41421
MeruyertA <145113292+MeruyertA@users.noreply.github.com>
2023-09-25 23:56:56 +0600

    Delete task8_2.py

--- task8_2.py
+++
@@ -1,9 +0,0 @@
-a = int(input())
-b = int(input())
-c = int(input())
-d = int(input())
-
-if (a == c and (b == d + 1 or b == d - 1)) or (b == d and (a == c + 1 or a == c - 1)) or ((a == c + 1 or a == c - 1) and (b == d + 1 or b == d - 1)):
-    print("YES")
-else:
-    print("NO")
